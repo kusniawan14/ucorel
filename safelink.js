0f3a7403038da9d7ddc6206110e24db1380c9a71
kusniawan14 <51787345+kusniawan14@users.noreply.github.com>
2019-06-19 06:25:15 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,10 +21,12 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://www.kompisafelinkv2.ga/2016/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="recognizing-functions-of-coordination.html"}
+if(x=="2"){xxx="do-you-know-that-food-can-cause.html"}
+if(x=="3"){xxx="mesothelioma-treatment.html"}
+if(x=="4"){xxx="treatment-of-mesothelioma-cancer.html"}
+if(x=="5"){xxx="who-gets-mesothelioma.html"}
+if(x=="6"){xxx="treating-malignant-mesothelioma.html"}a_to_ve[i].href="https://www.mesotheliomadisease.us/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
